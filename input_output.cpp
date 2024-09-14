5ba5fe2632d1ee805960cef4473a69a52eb75e9b
Dhruba Biswas <dhrubanew239@gmail.com>
2024-09-14 16:51:33 +0600

    C++ for DSA with Phitron

---
+++ input_output.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //printf("Hello world to DSA");-> for c
+    //std::cout<<"Hello world";
+    //std::cout<<"Hello\n"; 
+    //std::cout<<"Hello "<<"World";
+    //std::cout<<"Hello"<<std::endl<<"World";
+    int a=100,b=200;
+    //printf("%d", a);
+    //std::cout<<a<<std::endl<<b;
+    //cout<<a<<endl<<b;
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
